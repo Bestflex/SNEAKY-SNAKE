2f130939cac463c206bde19b0d95f38ac14edda7
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-03-29 14:39:56 +0000

    Delete jeu.cpp

--- jeu.cpp
+++
@@ -1,8 +0,0 @@
-#include "Jeu.h"
-
-Jeu::Jeu () : ter(), Serpent(), fruit() {
-	ter.mangePastille(pac.getX(),pac.getY());
-}
-
-Terrain & Jeu::getTerrain & () {return ter;}
-Serpent & Jeu::getSerpent & () {return ser;}
