95f98599f46769ecb2c4f90379389b4803a992fe
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-03-29 14:44:17 +0000

    Upload New File

---
+++ Fruit.cpp
@@ -0,0 +1,109 @@
+#include "Fruit.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <time.h>     // pour l’initialisation avec srand
+#include <stdlib.h>   // librairie contenant rand()
+
+
+using namespace std;
+
+Fruit::Fruit()
+{
+    srand(time(NULL));
+    unsigned int etat=rand()%10;
+    Terrain t;
+
+    if (etat==0) { f='p'; }
+    else if (etat==1) { f='a'; }
+    else if (etat==2) { f='b'; }
+    else if (etat==3) { f='c'; }
+    else if (etat==4) { f='d'; }
+    else if (etat==5) { f='e'; }
+    else if (etat==6) { f='f'; }
+    else if (etat==7) { f='g'; }
+    else if (etat==8) { f='h'; }
+    else if (etat==9) { f='i'; }
+
+    do{
+        x=rand()%(t.getDimX()-2) +1;
+        y=rand()%(t.getDimY()-2) +1;
+    }while(t.getXY(x,y)!=' ');
+
+}
+
+void Fruit::roleFruit(const char f,Serpent & s)
+{
+    if(f=='a')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='b')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='c')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='d')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='e')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='f')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='g')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='h')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+    else if(f=='i')
+    {
+        unsigned int a = s.getTaille()+1;
+        s.setTaille(a);
+    }
+}
+
+void Fruit::changerFruit(Fruit & f, Terrain & t)
+{
+    srand(time(NULL));
+    unsigned int etat=rand()%10;
+
+    if (etat==0) { f.f='p'; }
+    else if (etat==1) { f.f='a'; }
+    else if (etat==2) { f.f='b'; }
+    else if (etat==3) { f.f='c'; }
+    else if (etat==4) { f.f='d'; }
+    else if (etat==5) { f.f='e'; }
+    else if (etat==6) { f.f='f'; }
+    else if (etat==7) { f.f='g'; }
+    else if (etat==8) { f.f='h'; }
+    else if (etat==9) { f.f='i'; }
+
+    do{
+        f.x=rand()%(t.getDimX()-2) +1;
+        f.y=rand()%(t.getDimY()-2) +1;
+    }while(t.getXY(f.x,f.y)!=' ');
+}
+
+unsigned int Fruit::getX() const { return x; }
+unsigned int Fruit::getY() const { return y; }
+char Fruit::getCharF() const { return f; }
