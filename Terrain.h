d1b4b5ae893f3122cdc2c664b0ca59c395b8cb6a
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-03-29 14:47:33 +0000

    Upload New File

---
+++ Terrain.h
@@ -0,0 +1,27 @@
+#ifndef _TERRAIN_H
+#define _TERRAIN_H
+
+
+
+class Terrain{
+
+private:
+    unsigned int dimx;
+	unsigned int dimy;
+	char ter[100][100];
+
+public:
+    Terrain();
+
+    bool collision(unsigned int x,unsigned int y); // test collision avec mur et corps
+    void mangeFruit(const unsigned int x,const unsigned int y);
+    void poserFruit(unsigned int x,unsigned int y,char c);
+    bool estDirectionValide(unsigned int direction) const;
+
+    char getXY(unsigned int x,unsigned int y); //renvoie le type d'objet a une position x,y;
+    unsigned int getDimX() const;
+    unsigned int getDimY() const;
+
+};
+
+#endif
