d05ebac518fdb96838700abd94e39f3dd92aa3c8
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-03-31 17:54:23 +0000

    Replace Terrain.h

--- Terrain.h
+++ Terrain.h
@@ -15,7 +15,7 @@ public:
 
     bool collision(unsigned int x,unsigned int y); // test collision avec mur et corps
     void mangeFruit(const unsigned int x,const unsigned int y);
-    void poserFruit(unsigned int x,unsigned int y,char c);
+    //void poserFruit(unsigned int x,unsigned int y,char c);
     bool estDirectionValide(unsigned int direction) const;
 
     char getXY(unsigned int x,unsigned int y); //renvoie le type d'objet a une position x,y;
