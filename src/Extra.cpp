c970d0f80bf5382b4d86be59fe92ea1573fddd2c
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-04-25 09:44:49 +0000

    Upload New File

---
+++ src/Extra.cpp
@@ -0,0 +1,24 @@
+#include "Extra.h"
+
+#include <iostream>
+
+Extra::Extra()
+{
+    time1 = 0.2;
+    time2 = 0.2;
+
+    score1 = 0;
+    score2 = 0;
+}
+
+double Extra::getTime1() const { return time1; }
+void Extra::setTime1(double a) { time1=a; }
+
+double Extra::getTime2() const { return time2; }
+void Extra::setTime2(double a) { time2=a; }
+
+unsigned int Extra::getScore1() const { return score1; }
+void Extra::setScore1(unsigned int a) { score1=a; }
+
+unsigned int Extra::getScore2() const { return score2; }
+void Extra::setScore2(unsigned int a) { score2=a; }
