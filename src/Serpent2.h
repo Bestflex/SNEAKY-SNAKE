8322144f5d0c9b3cab79363958959ea5b163e8c9
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-04-25 09:48:38 +0000

    Upload New File

---
+++ src/Serpent2.h
@@ -0,0 +1,51 @@
+#ifndef _SERPENT2_H
+#define _SERPENT2_H
+
+
+#include "Terrain.h"
+
+const int MAX2 = 200;
+
+struct MorceauS2
+{
+    char c;
+    unsigned int x,y; //position du serpent2
+};
+
+class Serpent2{
+private:
+
+    MorceauS2 tab[MAX2];
+    unsigned int taille;
+    unsigned int direction;
+
+public :
+
+    Serpent2();
+
+    void droite(const Terrain & t, unsigned int dir);
+    void haut(const Terrain & t, unsigned int dir);
+    void gauche(const Terrain & t, unsigned int dir);
+    void bas(const Terrain & t, unsigned int dir);
+
+    void ajouterCorps();
+    void deplacementCorps();
+    void bougeAuto(unsigned int dir, const Terrain & t);
+
+    bool collisionS();
+
+    unsigned int getTaille();
+    void setTaille(unsigned int a);
+    unsigned int getX(unsigned int rang) const;
+    void setX(unsigned int a);
+    unsigned int getY(unsigned int rang) const;
+    void setY(unsigned int a);
+    char getC(unsigned int rang) const;
+    unsigned int getDirection() const;
+    void setDirection(unsigned int dir);
+   MorceauS2* getTab();
+
+
+};
+
+#endif // Serpent2.h
