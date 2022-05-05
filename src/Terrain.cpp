bf9d0989e91797c5c247b9c71f2d397730f33cae
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-05 08:29:51 +0000

    Replace Terrain.cpp

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -19,7 +19,7 @@ const char terrain1[10][30]{
 const char terrain2[10][30]{
 "#############################",
 "#                           #",
-"#                     #######",
+"#                     ##### #",
 "#    #                  #   #",
 "#     #                 #   #",
 "#      #                 #  #",
@@ -38,10 +38,22 @@ const char terrain3[10][30]{
 "#                   #       #",
 "# #        #######   #   ####",
 "#  #    #                   #",
-"#  #       #######          #",
+"#          #######          #",
 "#############################",
 };
 
+const char terrain4[10][30]{
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+"~                           ~",
+"~      ~~~~~~~~~~~~~~~~     ~",
+"~     ~                ~    ~",
+"~     ~                ~    ~",
+"~     ~                ~    ~",
+"~     ~               O~    ~",
+"~      ~~~~~~~~~~~~~~~~     ~",
+"~                          0~",
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+};
 
 Terrain::Terrain()
 {
@@ -53,24 +65,53 @@ Terrain::Terrain()
 	for(unsigned int x=0;x<dimx;++x)
     {
         for(unsigned int y=0;y<dimy;++y)
-        {     if (choixT==0){ter[x][y] = terrain1[y][x]; }
+        {
+              if (choixT==0){ter[x][y] = terrain1[y][x]; }
               if (choixT==1){ter[x][y] = terrain2[y][x]; }
               if (choixT==2){ter[x][y] = terrain3[y][x]; }
         }
     }
 }
 
-bool Terrain::collisionT(unsigned int x,unsigned int y) //collision avec le terrain
+void Terrain::changerTerrain()
+{
+    for(unsigned int i=0;i<dimx;++i)
+    {
+        for(unsigned int j=0;j<dimy;++j)
+        {
+              if (choixT==0){ter[i][j] = terrain1[j][i]; }
+              if (choixT==1){ter[i][j] = terrain2[j][i]; }
+              if (choixT==2){ter[i][j] = terrain3[j][i]; }
+              if (choixT==3){ter[i][j] = terrain4[j][i]; }
+        }
+    }
+}
+
+bool Terrain::collisionT(unsigned int x,unsigned int y) const //collision avec le terrain
 {
     if((ter[x][y]=='#')){ return true; }
     return false;
 }
 
-void Terrain::mangeFruit(const unsigned int x,const unsigned int y) { ter[x][y]=' '; }
+bool Terrain::collisionTSpecial(unsigned int x,unsigned int y) const//collision mur du terrain4
+{
+     if((ter[x][y]=='~')){ return true; }
+    return false;
+}
 
-//void Terrain::poserFruit(unsigned int x,unsigned int y,char c) { ter[x][y]= c; }
+bool Terrain::collisionTeleporteur1(unsigned int x,unsigned int y) const //collision mur du terrain4
+{
+    if(ter[x][y]=='0'){return true;}
+   return false;
+}
 
-char Terrain::getXY(unsigned int x, unsigned int y) { return ter[x][y]; }
+bool Terrain::collisionTeleporteur2(unsigned int x,unsigned int y)const //collision mur du terrain4
+{
+    if(ter[x][y]=='O'){return true;}
+   return false;
+}
+
+char Terrain::getXY(unsigned int x, unsigned int y) const { return ter[x][y]; }
 unsigned int Terrain::getDimX() const { return dimx; }
 unsigned int Terrain::getDimY() const { return dimy; }
 void Terrain::setChoix(unsigned int a) { choixT = a; }
