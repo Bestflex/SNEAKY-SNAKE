088283e12dcab42ce1dbc715c283360eda958419
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-05 08:32:31 +0000

    Replace Fruit.cpp

--- src/Fruit.cpp
+++ src/Fruit.cpp
@@ -31,8 +31,6 @@ void Fruit::roleFruit(Serpent & s, Terrain & t, Extra & e) // Determine l'effet 
     else if(f=='b') { s.setTaille(s.getTaille()+1); e.setScore1(score+2); } // augmente le score de 2
     else if(f=='c') { s.setTaille(s.getTaille()-1); e.setScore1(score-1); } // reduit la taille du serpent de 1
     else if(f=='d') { s.setTaille(s.getTaille()+1); e.setTime1(temps*2.0); e.setScore1(score+1); } // ralentit le serpent
-    else if(f=='e') { s.setTaille(s.getTaille()+1); e.setScore1(score+1); }
-    else if(f=='f') { s.setTaille(s.getTaille()+1); e.setScore1(score+1); }
     else if(f=='t') { s.setTaille(s.getTaille()+1); e.setScore1(score+1);   // teleporte le serpent à un lieux random
         do{
             a=rand()%(t.getDimY()-2) +1; b=rand()%(t.getDimY()-2) +1; // Position aleatoire
@@ -40,6 +38,7 @@ void Fruit::roleFruit(Serpent & s, Terrain & t, Extra & e) // Determine l'effet 
         }while(t.getXY(a,b)!=' '); } // Tant que la zone du terrain est vide
 
     else if(f=='p') { s.setTaille(s.getTaille()+1); e.setTime1(0.2); e.setScore1(score+1); } // Augmente juste la taille de 1
+    else if(f=='8') {t.setChoix(3),s.setX(1),s.setY(1),s.setDirection(0);} //on change de terrain, position et direction du serpent reinitialiser
 }
 
 void Fruit::roleFruit2(Serpent2 & s2, Terrain & t, Extra & e) // Determine l'effet de chaque fruit sur le Serpent 1
@@ -50,8 +49,6 @@ void Fruit::roleFruit2(Serpent2 & s2, Terrain & t, Extra & e) // Determine l'eff
     else if(f=='b') { s2.setTaille(s2.getTaille()+1); e.setTime2(0.2); e.setScore2(score+2); } // augmente le score de 2
     else if(f=='c') { s2.setTaille(s2.getTaille()-1); e.setTime2(0.1); e.setScore2(score-1); } // reduit la taille du serpent de 1
     else if(f=='d') { s2.setTaille(s2.getTaille()+1); e.setTime2(0.2); e.setScore2(score+1); } // ralentit le serpent
-    else if(f=='e') { s2.setTaille(s2.getTaille()+1); e.setTime2(0.1); e.setScore2(score+1); }
-    else if(f=='f') { s2.setTaille(s2.getTaille()+1); e.setTime2(0.1); e.setScore2(score+1); }
     else if(f=='t') { s2.setTaille(s2.getTaille()+1); e.setTime2(0.1); e.setScore2(score+1);   // teleporte le serpent à un lieux random
         do{
             a=rand()%(t.getDimY()-2) +1; b=rand()%(t.getDimY()-2) +1; // Position aleatoire
@@ -64,16 +61,14 @@ void Fruit::roleFruit2(Serpent2 & s2, Terrain & t, Extra & e) // Determine l'eff
 void Fruit::changerFruit(Terrain & t) // Permet de tirer un nouveau fruit aleatoirement
 {
     srand(time(NULL));
-    unsigned int etat=rand()%10; // Tire un chiffre aleatoirement entre 0 et 9
+    unsigned int etat=rand()%6; // Tire un chiffre aleatoirement entre 0 et 6
 
     if (etat==0) { f='p'; } // On associe chaque fruit à un etat
     else if (etat==1) { f='a'; }
     else if (etat==2) { f='b'; }
     else if (etat==3) { f='c'; }
     else if (etat==4) { f='d'; }
-    else if (etat==5) { f='e'; }
-    else if (etat==6) { f='f'; }
-    else if (etat==9) { f='t'; }
+    else if (etat==5) { f='t'; }
 
     do{
         x=rand()%(t.getDimX()-2) +1; // Position aleatoire
@@ -84,7 +79,7 @@ void Fruit::changerFruit(Terrain & t) // Permet de tirer un nouveau fruit aleato
 void Fruit::pomme(Terrain & t) // Permet de remmetre une pomme
 {
     srand(time(NULL));
-    f='d';
+    f='p';
     do{
         x=rand()%(t.getDimX()-2) +1; // Position aleatoire
         y=rand()%(t.getDimY()-2) +1; // Position aleatoire
