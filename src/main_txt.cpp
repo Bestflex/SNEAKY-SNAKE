2c4c68567f7329a273657c391ce2b3aa3c3eeab3
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-05 08:28:51 +0000

    Replace main_txt.cpp

--- src/main_txt.cpp
+++ src/main_txt.cpp
@@ -8,12 +8,9 @@ int main ( int argc, char** argv )
 {
     termClear();
 	Jeu jeu;
-	//txtBoucleSOLO(jeu);
     menu(jeu);
     termClear();
-    if(jeu.getGagnant()==1) { cout << "Bravo, vous avez gagne Serpent1" << endl; }
-    else if(jeu.getGagnant()==2) { cout << "Bravo Serpent2, vous avez gagne" << endl; }
-    else cout <<"egalite" <<endl;
+    jeu.messageFin();
 
 	return 0;
 }
