593e8f0702120359aa404bc2f06214467093b131
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-05 08:31:17 +0000

    Replace Jeu.h

--- src/Jeu.h
+++ src/Jeu.h
@@ -11,7 +11,7 @@ class Jeu{
 
 private:
     int gagnantVS; //0 =egalite, 1=joueur1, 2=joueur2
-    unsigned int panier; // choise entre posser que des pommes ou plusieurs fruits differents
+    unsigned int panier; // choisit entre poser que des pommes ou plusieurs fruits differents, et permet de savoir quel mode de jeux a été lancer
     Terrain t;
     Serpent s;
     Serpent2 s2;
@@ -28,14 +28,20 @@ public:
 
     bool actionClavier(const char touche);
     void actionsAutomatiques ();
-    void choixFruit(unsigned int x);
+    void choixFruit();
+    void terrainSpecial();
 
     unsigned int collisionSerpentCorps();
     bool collisionTete();
     bool finPartie();
+    void messageFin();
     int getGagnant();
     unsigned int getPanier();
     void setPanier(unsigned int x);
+    int directionAleaX();
+    int directionAleaY();
+    void jeuAuto ();
+
 
     const Terrain& getConstTerrain() const;
     const Serpent& getConstSerpent() const;
