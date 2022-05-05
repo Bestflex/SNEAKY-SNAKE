9c4042edbd4c10d689f38ecfc64ef1e8560f01b4
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-05 08:29:36 +0000

    Replace Terrain.h

--- src/Terrain.h
+++ src/Terrain.h
@@ -13,14 +13,17 @@ private:
 
 public:
     Terrain();
-
-    bool collisionT(unsigned int x,unsigned int y); // test collision avec mur et corps
+    bool collisionT(unsigned int x,unsigned int y) const; // test collision avec mur et corps
+    bool collisionTSpecial(unsigned int x,unsigned int y) const;
+    bool collisionTeleporteur1(unsigned int x,unsigned int y) const;
+    bool collisionTeleporteur2(unsigned int x,unsigned int y) const;
     void mangeFruit(const unsigned int x,const unsigned int y);
     //void poserFruit(unsigned int x,unsigned int y,char c);
-    char getXY(unsigned int x,unsigned int y); //renvoie le type d'objet a une position x,y;
+    char getXY(unsigned int x,unsigned int y)const; //renvoie le type d'objet a une position x,y;
     unsigned int getDimX() const;
     unsigned int getDimY() const;
     void setChoix(unsigned int a);
+    void changerTerrain();
 
 };
 
