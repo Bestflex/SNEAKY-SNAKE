2136406099c23e81060de285ff84fa992f95f8af
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-13 11:42:53 +0000

    Replace Extra.h

--- src/Extra.h
+++ src/Extra.h
@@ -1,28 +1,66 @@
 #ifndef _EXTRA_H
 #define _EXTRA_H
-
+/// Classe qui gere les temps et les scores
 class Extra{
 
 private:
+
+    /// Temps pour vitesse du premier Serpent
     double time1;
+    /// Temps pour vitesse du deuxieme Serpent (Serpent2)
     double time2;
+    /// Score du premier Serpent
     unsigned int score1;
+    /// Score du deuxieme Serpent (Serpent2)
     unsigned int score2;
 
 public:
 
+    /**
+     * @brief Constructeur par défaut de la classe:
+       Initialise les variables correspondants à la vitesse des deux serpents ainsi que le score des deux joueurs
+     */
     Extra();
 
-    double getTime1() const;
-    void setTime1(double a);
+    /**
+     * @brief Accesseur : récupère le temps du premier Serpent
+     */
+     double getTime1() const;
+
+    /**
+     * @brief Accesseur : récupère le temps du deuxieme Serpent
+     */
     double getTime2() const;
+
+    /**
+     * @brief Accesseur : récupère le score du premier Serpent
+     */
+    unsigned int getScore1() const;
+
+    /**
+     * @brief Accesseur : récupère le score du deuxieme Serpent
+     */
+    unsigned int getScore2() const;
+
+    /**
+     * @brief Mutateur : modifie le temps du premier Serpent
+     */
+    void setTime1(double a);
+
+    /**
+     * @brief Mutateur : modifie le temps du deuxieme Serpent
+     */
     void setTime2(double a);
 
-    unsigned int getScore1() const;
+    /**
+     * @brief Mutateur : modifie le score du premier Serpent
+     */
     void setScore1(unsigned int a);
-    unsigned int getScore2() const;
-    void setScore2(unsigned int a);
 
+    /**
+     * @brief Mutateur : modifie le score du deuxieme Serpent
+     */
+    void setScore2(unsigned int a);
 };
 
 #endif // Extra.h
