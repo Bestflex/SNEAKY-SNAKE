6afa31e8db03ce72a7ae85fe052e728d35e52ca3
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-13 11:44:07 +0000

    Replace Serpent.h

--- src/Serpent.h
+++ src/Serpent.h
@@ -5,44 +5,154 @@
 
 const int MAX = 200;
 
+/// Structure MorceauS
 struct MorceauS
 {
+    /// Charactere representant le serpent
     char c;
-    unsigned int x,y; //position du serpent
+    /// Coordonnées du morceau de serpent
+    unsigned int x,y;
 };
 
+/// Classe qui gere le premier Serpent
 class Serpent{
 private:
 
+    /// Tableau de Morceau de serpent
     MorceauS tab[MAX];
+    /// Taille du serpent
     unsigned int taille;
+    /// Direction du serpent
     unsigned int direction;
+    /// Vie du serpent
+    bool vie;
 
 public :
 
+    /**
+     * @brief Constructeur par défaut de la classe:
+       Initialise les variables du serpent ( Coordonnée de la tete, la taille du serpent, sa direction de base, le charactere representant la tete, la vie bonus à 0, ...)
+     */
     Serpent();
+
+    /**
+     * @brief Procedure qui deplace le serpent à droite.
+       La Procedure permet de quand elle est appelée de deplacer chaque membre du serpent d'une case vers la droite.
+       @param Terrain : la classe Terrain
+       @param dir : entié superieur ou egal à 0
+     */
     void droite(const Terrain & t, unsigned int dir);
+
+    /**
+     * @brief Procedure qui deplace le serpent à haut.
+       La Procedure permet de quand elle est appelée de deplacer chaque membre du serpent d'une case vers la haut.
+       @param Terrain : la classe Terrain
+       @param dir : entié superieur ou egal à 0
+     */
     void haut(const Terrain & t, unsigned int dir);
+
+    /**
+     * @brief Procedure qui deplace le serpent à gauche.
+       La Procedure permet de quand elle est appelée de deplacer chaque membre du serpent d'une case vers la gauche.
+       @param Terrain : la classe Terrain
+       @param dir : entié superieur ou egal à 0
+     */
     void gauche(const Terrain & t, unsigned int dir);
+
+    /**
+     * @brief Procedure qui deplace le serpent à bas.
+       La Procedure permet de quand elle est appelée de deplacer chaque membre du serpent d'une case vers la bas.
+       @param Terrain : la classe Terrain
+       @param dir : entié superieur ou egal à 0
+     */
     void bas(const Terrain & t, unsigned int dir);
 
+    /**
+     * @brief Procedure qui ajoute un corps au serpent.
+       La Procedure permet de quand elle est appelée d'ajouter un corps au serpent.
+     */
     void ajouterCorps();
-    void deplacementCorps();
-    void bougeAuto(unsigned int dir, const Terrain & t);
 
+    /**
+     * @brief Procedure qui deplace le corps du serpent.
+       La Procedure permet de quand elle est appelée de deplacer chaque membre du corps du serpent à la case anciennement occupé par un membre du serpent.
+     */
+    void deplacementCorps();
+
+    /**
+     * @brief Fonction qui test les collisions du serpent.
+       La Fonction qui test si le serpent rentre dans son corps à partir d'une taille egal à 5 (taille à partir de laquelle le serpent peut rentrer dans son corps.
+       @return true si collision sinon false
+     */
     bool collisionS();
 
+    /**
+     * @brief Accesseur : récupère la taille du serpent
+     */
     unsigned int getTaille();
-    void setTaille(unsigned int a);
-    unsigned int getX(unsigned int rang) const;
-    void setX(unsigned int a);
-    unsigned int getY(unsigned int rang) const;
-    void setY(unsigned int a);
-    char getC(unsigned int rang) const;
-    unsigned int getDirection() const;
-    void setDirection(unsigned int dir);
-    MorceauS* getTab();
 
+    /**
+     * @brief Accesseur : récupère la coordonnée x du morceau choisie
+       @param rang : entié superieur ou egal à 0 qui represente le rang du morceau du serpent choisie dans le tableau
+     */
+    unsigned int getX(unsigned int rang) const;
+
+    /**
+     * @brief Accesseur : récupère la coordonnée y du morceau choisie
+       @param rang : entié superieur ou egal à 0 qui represente le rang du morceau du serpent choisie dans le tableau
+     */
+    unsigned int getY(unsigned int rang) const;
+
+    /**
+     * @brief Accesseur : récupère le charactere c du morceau choisie
+       @param rang : entié superieur ou egal à 0 qui represente le rang du morceau du serpent choisie dans le tableau
+     */
+    char getC(unsigned int rang) const;
+
+    /**
+     * @brief Accesseur : récupère la direction du serpent
+     */
+    unsigned int getDirection() const;
+
+    /**
+     * @brief Accesseur : récupère le booleen indiquant si le serpent à une vie ou non
+     */
+    bool getVie() const ;
+
+    /**
+     * @brief Mutateur : modifie la taille du Serpent
+     */
+    void setTaille(unsigned int a);
+
+    /**
+     * @brief Mutateur : modifie la coordonnée x du Serpent
+     */
+    void setX(unsigned int a);
+
+    /**
+     * @brief Mutateur : modifie la coordonnée y du Serpent
+     */
+    void setY(unsigned int a);
+
+    /**
+     * @brief Mutateur : modifie la direction du Serpent
+     */
+    void setDirection(unsigned int dir);
+
+    /**
+     * @brief Mutateur : modifie le bool representant la vie du Serpent
+     */
+    void setVie(bool x);
+
+    /**
+     * @brief Mutateur : modifie la coordonnée x du corps du Serpent
+     */
+    void setCorpsX(unsigned int x);
+
+    /**
+     * @brief Mutateur : modifie la coordonnée y du corps du Serpent
+     */
+    void setCorpsY(unsigned int y);
 
 };
 
