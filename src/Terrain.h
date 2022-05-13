2d94c32264e498538ac4baa400feb7e22c001e65
BENAZIZ OUSSAMA p2007990 <oussama.benaziz@etu.univ-lyon1.fr>
2022-05-13 11:45:16 +0000

    Replace Terrain.h

--- src/Terrain.h
+++ src/Terrain.h
@@ -2,28 +2,70 @@
 #define _TERRAIN_H
 
 
-
+/// Classe qui gere le terrain
 class Terrain{
 
 private:
+    /// Dimension x du terrain
     unsigned int dimx;
+	/// Dimension y du terrain
 	unsigned int dimy;
+	/// Choix du terrain
 	unsigned int choixT;
+	/// Tableau 2d pour le terrain
 	char ter[100][100];
 
 public:
+
+    /**
+     * @brief Constructeur par défaut de la classe:
+       Initialise les dimension du terrain et choisie un terrain aleatoirement parmi 4 terrains
+     */
     Terrain();
-    bool collisionT(unsigned int x,unsigned int y) const; // test collision avec mur et corps
-    bool collisionTSpecial(unsigned int x,unsigned int y) const;
+
+    /**
+     * @brief Fonction qui test les collision avec les murs.
+       La Fonction qui test si les serpents rentre dans le mur.
+       @param x : entié superieur ou egal à 0 (coordonnée x saisie)
+       @param y : entié superieur ou egal à 0 (coordonnée y saisie)
+       @return true si collision sinon false
+     */
+    bool collisionT(unsigned int x,unsigned int y) const;
+
+    /**
+     * @brief Fonction qui test la collision avec le teleporteur1.
+       La Fonction qui test si les serpents rentre dans le teleporteur1.
+       @param x : entié superieur ou egal à 0 (coordonnée x saisie)
+       @param y : entié superieur ou egal à 0 (coordonnée y saisie)
+       @return true si collision sinon false
+     */
     bool collisionTeleporteur1(unsigned int x,unsigned int y) const;
+
+    /**
+     * @brief Fonction qui test la collision avec le teleporteur2.
+       La Fonction qui test si les serpents rentre dans le teleporteur2.
+       @param x : entié superieur ou egal à 0 (coordonnée x saisie)
+       @param y : entié superieur ou egal à 0 (coordonnée y saisie)
+       @return true si collision sinon false
+     */
     bool collisionTeleporteur2(unsigned int x,unsigned int y) const;
-    void mangeFruit(const unsigned int x,const unsigned int y);
-    //void poserFruit(unsigned int x,unsigned int y,char c);
-    char getXY(unsigned int x,unsigned int y)const; //renvoie le type d'objet a une position x,y;
+
+    /**
+     * @brief Accesseur : récupère le charactere ayant pour coordonnées x et y dans le terrain
+       @param x : entié superieur ou egal à 0 (coordonnée x saisie)
+       @param y : entié superieur ou egal à 0 (coordonnée y saisie)
+     */
+    char getXY(unsigned int x,unsigned int y)const;
+
+    /**
+     * @brief Accesseur : récupère la coordonnée x du terrain
+     */
     unsigned int getDimX() const;
+
+    /**
+     * @brief Accesseur : récupère la coordonnée y du terrain
+     */
     unsigned int getDimY() const;
-    void setChoix(unsigned int a);
-    void changerTerrain();
 
 };
 
